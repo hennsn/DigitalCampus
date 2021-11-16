fcbb176c13dadaae31a9bcbde11b6f3c7b456eb9
Antonio Noack <antonio-noack@gmx.de>
2021-11-16 13:24:22 +0100

    first three.js sample; including a loaded gltf-draco file :D (however without materials currently :/)

---
+++ js/main.js
@@ -0,0 +1,110 @@
+
+import * as THREE from 'https://cdn.skypack.dev/three@0.134.0'
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.134.0/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.134.0/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'https://cdn.skypack.dev/three@0.134.0/examples/jsm/loaders/DRACOLoader.js'
+import Stats from 'https://cdn.skypack.dev/three@0.134.0/examples/jsm/libs/stats.module'
+
+// conventions (may be altered, if sb hates them 😂):
+// x = left/right
+// y = up/down (may change, because z is up in Blender)
+// z = forward/backward
+
+// tabs instead of spaces for identation, so every user can set the identation width themselves
+// single and multi line comments are fine
+// no semicolons, because they are not really required in JavaScript 😄. Except somebody really loves them
+
+// named functions probably should have the "function" keyword, while annonymous functions should be lambda expressions
+
+const near = 0.1  // near clipping plane: closer pixels are invisible
+const far  = 1000 // far clipping plane: farther pixels/objects are invisible
+const fov  = 75   // fov in degrees, on the y axis
+
+// camera
+const camera = new THREE.PerspectiveCamera(fov, window.innerWidth/window.innerHeight, near, far)
+camera.position.y = 1
+camera.position.z = 5
+
+// renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+////////////////////////////
+// scene + sample objects //
+////////////////////////////
+
+const scene = new THREE.Scene()
+// Configure and create Draco decoder.
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('js/libs/draco/')
+// who can get this working over the CDN? current error message: "Uncaught SyntaxError: Unexpected token 'export'"
+// dracoLoader.setDecoderPath('https://cdn.skypack.dev/three@0.134.0/examples/js/libs/draco/')
+dracoLoader.setDecoderConfig({ type: 'js' })
+const glTFLoader = new GLTFLoader()
+glTFLoader.setDRACOLoader(dracoLoader)
+
+// temporary sky color as long as we don't have a HDR for that
+scene.background = new THREE.Color(0xbfd1e5)
+
+const geometry = new THREE.BoxGeometry()
+const material = new THREE.MeshBasicMaterial({ color: 0x77ff33 })
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
+
+// sun light from above
+// can support shadows, but we need a ground to project them onto, and we need to configure them (best depending on the hardware capabilities)
+const sun = new THREE.DirectionalLight(0xffffff);
+sun.rotation.x = -30*3.14/180
+scene.add(sun)
+
+// ambient light from below
+const ambient = new THREE.HemisphereLight(0xffffff, 0x444444);
+scene.add(ambient)
+
+// works, but somehow the materials are lost :/
+glTFLoader.load('models/samples/draco-monkey.glb',
+	(gltf) => {
+		const model = gltf.scene
+		model.position.y = 1.5
+		scene.add(model)
+	}, undefined,
+	(error) => { console.error(error) }
+)
+
+////////////////////////////////
+// listeners for interactions //
+////////////////////////////////
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.target.set(0, 1, 0) // orbit center
+controls.update()// compute transform for 1st frame
+
+// adjust the aspect ratio as needed:
+window.addEventListener("resize", (event) => {
+	camera.aspect = window.innerWidth / window.innerHeight
+	camera.updateProjectionMatrix()
+	renderer.setSize(window.innerWidth, window.innerHeight)
+})
+
+// todo webxr button & support
+
+// todo top-down map?
+
+// todo GPS input to show where we are on the map
+
+// for debugging: fps/frame-time/memory usage
+// browsers are typically locked at the screen refresh rate, so 60 fps (in my case) is perfect
+const stats = Stats()
+document.body.appendChild(stats.dom)
+
+function mainLoop(){
+	requestAnimationFrame(mainLoop)
+	
+	// animation / physics stuff goes here
+	
+	stats.update()
+	renderer.render(scene, camera)
+}
+mainLoop()
+
