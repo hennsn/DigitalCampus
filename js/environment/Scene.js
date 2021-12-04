841d5d9d59115e760f86e57237a96956de43e94a
Antonio Noack <antonio-noack@gmx.de>
2021-12-04 14:27:44 +0100

    added the main campus as a rough, untextured model (generated using Blender-OSM plugin)

--- js/environment/Scene.js
+++ js/environment/Scene.js
@@ -13,6 +13,14 @@ function fillScene(scene){
 		scene.add(model)
 	}, undefined, printError)
 	
+	glTFLoader.load('models/samples/campus-joined.glb', (gltf) => {
+		const model = window.abbeanum = gltf.scene
+		placeLatLonObject(model, 'City Center', 50.9279284 + 0.0001, 11.5829607 - 0.00016, 150, 0)
+		var scale = 1 // a guess
+		model.scale.set(scale, scale, scale)
+		scene.add(model)
+	}, undefined, printError)
+	
 }
 
 export { fillScene }
