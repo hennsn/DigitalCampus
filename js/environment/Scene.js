f1d218bb722d7196974976ec1ed5f134d36b2ab9
oranium <aron.gaden@gmail.com>
2021-12-06 11:06:32 +0100

    load updated Abbeanum model and increase size a little

--- js/environment/Scene.js
+++ js/environment/Scene.js
@@ -5,10 +5,10 @@ import { printError } from '../UserInterface.js'
 function fillScene(scene){
 	
 	// our first model: the Abbeanum, Fröbelstieg 1
-	glTFLoader.load('models/samples/Abbeanum (teils texturiert).glb', (gltf) => {
+	glTFLoader.load('models/samples/Abbeanum.glb', (gltf) => {
 		const model = window.abbeanum = gltf.scene
 		placeLatLonObject(model, 'Abbeanum', 50.9339769, 11.5804391, 182, +15)
-		var scale = 2.8 // a guess
+		var scale = 1.3 // a guess
 		model.scale.set(scale, scale, scale)
 		scene.add(model)
 	}, undefined, printError)
