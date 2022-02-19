13c6dca03a1ec4f994ee9b21da7c55e33ba5cfd3
mar10-9 <94121007+mar10-9@users.noreply.github.com>
2022-02-19 15:29:37 +0100

    Quiz
    Abbeanum-Quiz (vorerst über Taste k aufrufbar, schönheitsarbeiten noch offen)

--- js/interactions/Interactions.js
+++ js/interactions/Interactions.js
@@ -30,6 +30,8 @@ let inventoryOpen = false
 let isBlocked = false
 //boolean for picture display
 let infoPictureOpen = false;
+//boolean for quiz
+let quizOpen = false;
 
 //array für alle modelle die wir einsammeln
 const inInventory = ["Handy", "USB Stick"]
@@ -426,6 +428,16 @@ function createInteractions(scene, camera, renderer, mouse){
 						infoPictureOpen = false;
 					}
 					break;
+
+				case 'k':	// display Abbeanum-Quiz (vorerst hierüber)
+					if(quizOpen == false){
+						document.getElementById("abbeanum-quiz").style.visibility = 'visible';
+						quizOpen = true;
+					}else{
+						document.getElementById("abbeanum-quiz").style.visibility = 'hidden';
+						quizOpen = false;
+					}
+					break;
 			}
 		}
 	}
@@ -813,6 +825,124 @@ function close_image(imgID){
 	b.parentNode.removeChild(b);
 }
 
+// ---- Quiz ----- (derzeit über 'k')
+const quizStartBtn = document.getElementById('quiz-start');
+const quizNextBtn = document.getElementById('quiz-next');
+const quizExitBtn = document.getElementById('quiz-exit');
+const quizQuestionContainer = document.getElementById('quiz-container');
+const quizQuestionElem = document.getElementById('question');
+const quizAnswerBtnElem = document.getElementById('quiz-answer-btns')
+let quizQuestionIndex;
+
+quizStartBtn.addEventListener('click', quizStart);
+quizNextBtn.addEventListener('click', () => {
+	quizQuestionIndex++;
+	quizNextQuestion();
+});
+quizExitBtn.addEventListener('click', () => {
+	document.getElementById("abbeanum-quiz").style.visibility = 'hidden';
+	quizOpen = false;
+});
+
+
+function quizStart(){	
+	// start quiz, hide start-btn, show questions
+	quizStartBtn.classList.add('hide');
+	quizExitBtn.classList.add('hide');
+	quizQuestionIndex = 0;
+	quizQuestionContainer.classList.remove('hide');
+	quizNextQuestion();
+}
+
+function quizNextQuestion(){	
+	resetAnswers();
+	display_question(quizQuestions[quizQuestionIndex]);
+}
+
+function display_question(question){
+	quizQuestionElem.innerText = question.question;
+	question.answers.forEach(answer => {
+		const button = document.createElement('button');
+		button.innerText = answer.text;
+		button.classList.add('btn');
+		if(answer.correct){
+			button.dataset.correct = answer.correct;	// da wir strings vergleichen
+		}
+		button.addEventListener('click', quizAnswer);
+		quizAnswerBtnElem.appendChild(button);
+	})
+}
+
+function resetAnswers(){
+	clearGivenAnswer(document.body);
+	quizNextBtn.classList.add('hide');
+	while(quizAnswerBtnElem.firstChild){
+		quizAnswerBtnElem.removeChild(quizAnswerBtnElem.firstChild);
+	}
+}
+
+function quizAnswer(e){		// check the answer
+	const clickedBtn = e.target;
+	const correct = clickedBtn.dataset.correct;	// check the dataset
+	setGivenAnswer(document.body, correct);	
+	Array.from(quizAnswerBtnElem.children).forEach(button => {
+		setGivenAnswer(button, button.dataset.correct)
+	})
+	if (quizQuestions.length > quizQuestionIndex +1){	
+		quizNextBtn.classList.remove('hide');
+	} else {
+		quizStartBtn.innerText = 'Restart';
+		quizStartBtn.classList.remove('hide');
+		quizExitBtn.classList.remove('hide');
+	}
+}
+
+function setGivenAnswer(element, correct){
+	clearGivenAnswer(element);
+	if (correct){
+		element.classList.add('correct');
+	} else {
+		element.classList.add('wrong');
+	}
+}
+
+function clearGivenAnswer(element){
+	element.classList.remove('correct');
+	element.classList.remove('wrong');
+}
+
+const quizQuestions = [
+	{
+		question: 'Wer beauftragte den Bau des Abbeanums?',
+		answers: [
+			{ text: 'Carl-Zeiss-Stiftung', correct: true }, 
+			{ text: 'JenOptik', correct: false},
+			{ text: 'Ernst-Abbe-Hochschule', correct: false}
+		]
+	},{
+		question: 'Wer war der Architekt diese Gebäudes?',
+		answers: [
+			{text: 'Hugo Hartung', correct: false},
+			{text: 'Ernst Abbe', correct: false},
+			{text: 'Ernst Neufert', correct: true}
+		]
+	},{
+		question: 'Welcher Bau-Stil liegt diesem Gebäude zugrunde?',
+		answers: [
+			{text: 'Bauhaus-Stil', correct: true},
+			{text: 'Funktionalismus', correct: false},
+			{text: 'Expressionismus', correct: false},
+		]
+	},{
+		question: 'Zu welcher Fakultät gehört das Abbeanum?',
+		answers:[
+			{text: 'Physikalisch-Astronomische Fakultät', correct: true},
+			{text: 'Philosophische Fakultät', correct: false},
+			{text: 'Chemisch-Geowissenschaftliche Fakultät', correct: false}
+		]
+	}
+]
+// --- (ende quiz) ---
 
 
 export { createInteractions, handleInteractions, inInventory, printInventory, hideInventory, interactables, keyWasPressed, wasClicked, blockUserInput, allowUserInput, findElement }
